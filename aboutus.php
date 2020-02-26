53f2697f81e022b98ac0f31b629a5a4c0664dbd2
ayush0711 <ayush.mittal_cs17@gla.ac.in>
2020-02-26 22:20:13 +0530

    there is link ,css and scripts code

--- aboutus.php
+++ aboutus.php
@@ -58,3 +58,71 @@ while($row=mysqli_fetch_array($query))
  $query=mysqli_query($con,"select tbfood.foodname,tbfood.fldvendor_id,tbfood.cost,tbfood.cuisines,tbfood.fldimage,tblcart.fld_cart_id,tblcart.fld_product_id,tblcart.fld_customer_id from tbfood inner  join tblcart on tbfood.food_id=tblcart.fld_product_id where tblcart.fld_customer_id='$cust_id'");
   $re=mysqli_num_rows($query);
 ?>
+
+<html>
+  <head>
+     <title>Home</title>
+	 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+	 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	 <link href="https://fonts.googleapis.com/css?family=Great+Vibes|Permanent+Marker" rel="stylesheet">
+     
+	 <style>
+	 .carousel-item {
+  height: 100vh;
+  min-height: 350px;
+  background: no-repeat center center scroll;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+	 }
+  body{
+     background-image:url("img/main_spice2.jpg");
+	 background-repeat: no-repeat;
+	 background-attachment: fixed;
+	  background-position: center;
+  
+}
+
+	 </style>
+	 
+	 
+	 <script>
+	 //search product function
+            $(document).ready(function(){
+	
+	             $("#search_text").keypress(function()
+	                      {
+	                       load_data();
+	                       function load_data(query)
+	                           {
+		                        $.ajax({
+			                    url:"fetch.php",
+			                    method:"post",
+			                    data:{query:query},
+			                    success:function(data)
+			                                 {
+				                               $('#result').html(data);
+			                                  }
+		                                });
+	                             }
+	
+	                           $('#search_text').keyup(function(){
+		                       var search = $(this).val();
+		                           if(search != '')
+		                               {
+			                             load_data(search);
+		                                }
+		                            else
+		                             {
+			                         load_data();			
+		                              }
+	                                });
+	                              });
+	                            });
+</script>
