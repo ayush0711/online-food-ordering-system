22dc03c869e9a2e6f35d52d42a299de8b6385026
ayush0711 <ayush.mittal_cs17@gla.ac.in>
2020-02-26 22:21:37 +0530

    this is navigation bar code

--- aboutus.php
+++ aboutus.php
@@ -126,3 +126,78 @@ while($row=mysqli_fetch_array($query))
 	                              });
 	                            });
 </script>
+<style>
+ul li {list-style:none;}
+ ul li a{color:black;text-decoration:none;}
+ ul li a:hover{color:black;text-decoration:none;}
+</style>
+  </head>
+  
+    
+	<body>
+	<!--navbar start-->
+
+<div id="result" style="position:fixed;top:100; right:50;z-index: 3000;width:350px;background:white;"></div>
+<!--navbar ends-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  
+    <a class="navbar-brand" href="index.php"><span style="color:green;font-family: 'Permanent Marker', cursive;">Food Hunt</span></a>
+    <?php
+	if(!empty($cust_id))
+	{
+	?>
+	<a class="navbar-brand" style="color:black; text-decoratio:none;"><i class="far fa-user"><?php if(isset($cust_id)) { echo $qqr['fld_name']; }?></i></a>
+	<?php
+	}
+	?>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+	
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home
+                
+              </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="aboutus.php">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="services.php">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.php">Contact</a>
+        </li>
+		<li class="nav-item">
+		  <form method="post">
+          <?php
+			if(empty($cust_id))
+			{
+			?>
+			<a href="form/index.php?msg=you must be login first"><span style="color:red; font-size:30px;"><i class="fa fa-shopping-cart" aria-hidden="true"><span style="color:red;" id="cart"  class="badge badge-light">0</span></i></span></a>
+			
+			&nbsp;&nbsp;&nbsp;
+			<button class="btn btn-outline-danger my-2 my-sm-0" name="login" type="submit">Log In</button>&nbsp;&nbsp;&nbsp;
+            <?php
+			}
+			else
+			{
+			?>
+			<a href="form/cart.php"><span style=" color:green; font-size:30px;"><i class="fa fa-shopping-cart" aria-hidden="true"><span style="color:green;" id="cart"  class="badge badge-light"><?php if(isset($re)) { echo $re; }?></span></i></span></a>
+			<button class="btn btn-outline-success my-2 my-sm-0" name="logout" type="submit">Log Out</button>&nbsp;&nbsp;&nbsp;
+			<?php
+			}
+			?>
+			</form>
+        </li>
+		<li class="nav-item">
+		
+		  
+		</li>
+      </ul>
+	  
+    </div>
+	
+</nav>
