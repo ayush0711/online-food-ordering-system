6e6fb72ac12f35f4782c70530f692ea00e2565ae
AnujSaxena19 <19anujsaxena19@gmail.com>
2020-04-03 23:06:28 +0530

    Html code for services page

--- services.php
+++ services.php
@@ -132,4 +132,97 @@ ul li a:hover{text-decoration:none;}
 
 
 <div id="result" style="position:fixed;top:100; right:50;z-index: 3000;width:350px;background:white;"></div>
+<!--navbar start-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  
+    <a class="navbar-brand" href="index.php"><span style="color:green;font-family: 'Permanent Marker', cursive;">Food Hunt</span></a>
+    <?php
+	if(!empty($cust_id))
+	{
+	?>
+	<a class="navbar-brand" style="color:black; text-decoratio:none;"><i class="far fa-user"><?php if(isset($cust_id)) { echo $qqr['fld_name']; }?></i></a>
+	<?php
+	}
+	?>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+	
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home
+                
+              </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="aboutus.php">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="services.php">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.php">Contact</a>
+        </li>
+		<li class="nav-item">
+		  <form method="post">
+          <?php
+			if(empty($cust_id))
+			{
+			?>
+			<a href="form/index.php?msg=you must be login first"><span style="color:red; font-size:30px;"><i class="fa fa-shopping-cart" aria-hidden="true"><span style="color:red;" id="cart"  class="badge badge-light">0</span></i></span></a>
+			
+			&nbsp;&nbsp;&nbsp;
+			<button class="btn btn-outline-danger my-2 my-sm-0" name="login" type="submit">Log In</button>&nbsp;&nbsp;&nbsp;
+            <?php
+			}
+			else
+			{
+			?>
+			<a href="form/cart.php"><span style=" color:green; font-size:30px;"><i class="fa fa-shopping-cart" aria-hidden="true"><span style="color:green;" id="cart"  class="badge badge-light"><?php if(isset($re)) { echo $re; }?></span></i></span></a>
+			<button class="btn btn-outline-success my-2 my-sm-0" name="logout" type="submit">Log Out</button>&nbsp;&nbsp;&nbsp;
+			<?php
+			}
+			?>
+			</form>
+        </li>
+		<li class="nav-item">
+		
+		  
+		</li>
+      </ul>
+	  
+    </div>
+	
+</nav>
+<!--navbar ends-->
+<div class="container-fluid">
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+   <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      
+    </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="img/Customer-Support.jpg" alt="First slide">
+    
+	</div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/f1.jpg" alt="Second slide">
+      
+	</div>
+    
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
 
+ 
