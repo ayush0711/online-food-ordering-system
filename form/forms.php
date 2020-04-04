6b130789c4956f412327cfeb019dd60d1f8fd1a6
abhisheksingh102 <abhishek.singh_cs17@gla.ac.in>
2020-04-04 23:57:57 +0530

    login Section

--- form/forms.php
+++ form/forms.php
@@ -101,3 +101,62 @@ if(isset($register))
 		</style>
 </head>
 
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  
+    <a class="navbar-brand" href="index.php"><span style="color:green;font-family: 'Permanent Marker', cursive;">Food Hunt</span></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+	
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="../index.php">Home
+                
+              </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../aboutus.php">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../services.php">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../contact.php">Contact</a>
+        </li>
+		
+		
+      </ul>
+	  
+    </div>
+	
+</nav>
+<div class="middle" style=" position:fixed; padding:40px; border:1px solid #ED2553; left:30%; top:30%; width:400px;">
+       <ul class="nav nav-tabs nabbar_inverse" id="myTab" style="background:#ED2553;border-radius:10px 10px 10px 10px;" role="tablist">
+          <li class="nav-item">
+             <a class="nav-link active" id="home-tab" data-toggle="tab" href="#login" role="tab" aria-controls="home" aria-selected="true">Home</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" id="profile-tab" data-toggle="tab" href="#signup" role="tab" aria-controls="profile" aria-selected="false">Create New Account</a>
+          </li>
+       </ul>
+	   <br><br>
+	   <div class="tab-content" id="myTabContent">
+	   <!--login Section-- starts-->
+            <div class="tab-pane fade show active" id="login" role="tabpanel" aria-labelledby="home-tab">
+			  <form method="post" enctype="multipart/form-data">
+                    <div class="form-group">
+                      <label for="email">Email address:</label>
+                      <input type="email" class="form-control" name="email" id="email" required/>
+                    </div>
+                   <div class="form-group">
+                      <label for="pwd">Password:</label>
+                     <input type="password" name="password" class="form-control" id="pwd" required/>
+                   </div>
+ 
+                  <button type="submit" name="login" style="background:#ED2553; border:1px solid #ED2553;" class="btn btn-primary">Login In</button>
+                  <div class="footer" style="color:red;"><?php if(isset($ermsg)) { echo $ermsg; }?><?php if(isset($ermsg2)) { echo $ermsg2; }?></div>
+			 </form>
+			</div>
+			<!--login Section-- ends-->
