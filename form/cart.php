19d1749604f8e73ce501c5a8fc55bcf99f44f86f
AnujSaxena19 <19anujsaxena19@gmail.com>
2020-04-04 22:51:31 +0530

    This page is for adding item to the cart

---
+++ form/cart.php
@@ -0,0 +1,179 @@
+<?php
+session_start();
+
+extract($_REQUEST);
+include("../connection.php");
+$gtotal=array();
+$ar=array();
+$total=0;
+if(isset($_GET['product']))//product id
+{
+	$product_id=$_GET['product'];
+}
+else
+{
+	$product_id="";
+}
+ if(isset($_SESSION['cust_id']))
+ {
+ $cust_id=$_SESSION['cust_id'];
+ $qq=mysqli_query($con,"select * from tblcustomer where fld_email='$cust_id'");
+	 $qqr= mysqli_fetch_array($qq);
+ }
+if(empty($cust_id))
+{
+	header("location:index.php?msg=you must login first");
+}
+if(!empty($product_id && $cust_id ))
+{
+if(mysqli_query($con,"insert into tblcart (fld_product_id,fld_customer_id) values ('$product_id','$cust_id') "))
+{
+	echo "success";
+	$product_id="";
+	header("location:cart.php");
+}
+else
+{
+	echo "failed";
+}
+}
+if(isset($del))
+{
+	
+	//echo $del;
+	if(mysqli_query($con,"delete from tblcart where fld_cart_id='$del' && fld_customer_id='$cust_id'"))
+	{
+		header("location:deletecart.php");
+	}
+	
+}
+ 
+ 
+ if(isset($logout))
+ {
+	 session_destroy();
+	 
+	 header("location:../index.php");
+ }
+ if(isset($login))
+ {
+	 session_destroy();
+	 
+	 header("location:index.php");
+ }
+ 
+ //update section
+  $cust_details=mysqli_query($con,"select * from tblcustomer where fld_email='$cust_id'");
+  $det_res=mysqli_fetch_array($cust_details);
+  $fld_name=$det_res['fld_name'];
+  $fld_email=$det_res['fld_email'];
+  $fld_mobile=$det_res['fld_mobile'];
+  $fld_password=$det_res['password'];
+  if(isset($update))
+  {
+	   
+	 if(mysqli_query($con,"update tblcustomer set fld_name='$name',fld_mobile='$mobile',password='$pswd' where fld_email='$fld_email'"))
+      {
+	   header("location:customerupdate.php");
+	  }
+  }
+  
+  $query=mysqli_query($con,"select tbfood.foodname,tbfood.fldvendor_id,tbfood.cost,tbfood.cuisines,tbfood.fldimage,tblcart.fld_cart_id,tblcart.fld_product_id,tblcart.fld_customer_id from tbfood inner  join tblcart on tbfood.food_id=tblcart.fld_product_id where tblcart.fld_customer_id='$cust_id'");
+  $re=mysqli_num_rows($query);
+  
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Cart </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+     
+	 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+     <style>
+		ul li{list-style:none;}
+		ul li a {color:black;text-decoration:none; }
+		ul li a:hover {color:black;text-decoration:none; }
+		
+	 </style>
+	 <script>
+		  function del(id)
+		  {
+			  if(confirm('are you sure you want to cancel order')== true)
+			  {
+				  window.location.href='cancelorder.php?id=' +id;
+			  }
+		  }
+		</script>
+
+</head>
+<body>
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  
+    <a class="navbar-brand" href="../index.php"><span style="color:green;font-family: 'Permanent Marker', cursive;">Food Hunt</span></a>
+    <?php
+	if(!empty($cust_id))
+	{
+	?>
+	<a class="navbar-brand" style="color:black; text-decoratio:none;"><i class="far fa-user"><?php if(isset($cust_id)) { echo $qqr['fld_name']; }?></i></a>
+	<?php
+	}
+	?>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+	
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="../index.php">Home
+                
+              </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../aboutus.php">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../services.php">Services</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../contact.php">Contact</a>
+        </li>
+		<li class="nav-item">
+		  <form method="post">
+          <?php
+			if(empty($cust_id))
+			{
+			?>
+			<span style="color:black; font-size:30px;"><i class="fa fa-shopping-cart" aria-hidden="true"><span style="color:red;" id="cart"  class="badge badge-light">4</span></i></span>
+			
+			&nbsp;&nbsp;&nbsp;
+			<button class="btn btn-outline-danger my-2 my-sm-0" name="login">Log In</button>&nbsp;&nbsp;&nbsp;
+            <?php
+			}
+			else
+			{
+			?>
+			<a href="cart.php"><span style="color:green; font-size:30px;"><i class="fa fa-shopping-cart" aria-hidden="true"><span style="color:green;" id="cart"  class="badge badge-light"><?php if(isset($re)) echo $re; ?></span></i></span></a>
+			<button class="btn btn-outline-success my-2 my-sm-0" name="logout" type="submit">Log Out</button>&nbsp;&nbsp;&nbsp;
+			<?php
+			}
+			?>
+			</form>
+        </li>
+		
+		
+      </ul>
+	  
+    </div>
+	
