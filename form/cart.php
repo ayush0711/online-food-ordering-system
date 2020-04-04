ab3d6c8cd89b583bf7c6f902b88aad3beb8ef055
AnujSaxena19 <19anujsaxena19@gmail.com>
2020-04-04 22:54:37 +0530

    code for adding item to the cart

--- form/cart.php
+++ form/cart.php
@@ -177,3 +177,188 @@ if(isset($del))
 	  
     </div>
 	
+    </nav>
+
+<!--navbar ends-->
+
+
+<br><br>
+<div class="middle" style="  padding:60px; border:1px solid #ED2553;  width:100%;">
+       <!--tab heading-->
+	   <ul class="nav nav-tabs nabbar_inverse" id="myTab" style="background:#ED2553;border-radius:10px 10px 10px 10px;" role="tablist">
+          <li class="nav-item">
+             <a class="nav-link active" style="color:#BDDEFD;" id="viewitem-tab" data-toggle="tab" href="#viewitem" role="tab" aria-controls="viewitem" aria-selected="true">View Cart</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" style="color:#BDDEFD;" id="manageaccount-tab" data-toggle="tab" href="#manageaccount" role="tab" aria-controls="manageaccount" aria-selected="false">Account Settings</a>
+          </li>
+		  <li class="nav-item">
+              <a class="nav-link" style="color:#BDDEFD;" id="orders-tab" data-toggle="tab" href="#orders" role="tab" aria-controls="orders" aria-selected="false">Orders</a>
+          </li>
+		  
+       </ul>
+	   <br><br>
+	<!--tab 1 starts-->   
+	   <div class="tab-content" id="myTabContent">
+	   
+            <div class="tab-pane fade show active" id="viewitem" role="tabpanel" aria-labelledby="home-tab">
+                 <table class="table">
+                  <tbody>
+	               <?php
+	                  $query=mysqli_query($con,"select tbfood.foodname,tbfood.fldvendor_id,tbfood.cost,tbfood.cuisines,tbfood.fldimage,tblcart.fld_cart_id,tblcart.fld_product_id,tblcart.fld_customer_id from tbfood inner  join tblcart on tbfood.food_id=tblcart.fld_product_id where tblcart.fld_customer_id='$cust_id'");
+	                  $re=mysqli_num_rows($query);
+	                   if($re)
+	                    {
+		                 while($res=mysqli_fetch_array($query))
+		                  {
+			                $vendor_id=$res['fldvendor_id'];
+			               $v_query=mysqli_query($con,"select * from tblvendor where fldvendor_id='$vendor_id'");
+			               $v_row=mysqli_fetch_array($v_query);
+			               $em=$v_row['fld_email'];
+			               $nm=$v_row['fld_name'];
+	                ?>
+                      <tr>
+                         <td><image src="../image/restaurant/<?php echo $em."/foodimages/".$res['fldimage'];?>" height="80px" width="100px"></td>
+                         <td><?php echo $res['foodname'];?></td>
+                         <td><?php echo "RS ".$res['cost'];?></td>
+                         <td><?php echo $res['cuisines'];?></td>
+                         <td><?php echo $nm; ?></td>
+		                <form method="post" enctype="multipart/form-data">
+                           <td><button type="submit" name="del"  value="<?php echo $res['fld_cart_id']?>" class="btn btn-danger">Delete</button></td>
+                        </form>
+                        <td><?php $total=$total+$res['cost']; $gtotal[]=$total;  ?></td>
+                      </tr>
+					  
+					  
+                   <?php
+	                    }
+						?>
+						<tr>
+					  <td>
+					  <h5 style="color:red;">Grand total</h5>
+					  </td>
+					  <td>
+					  <h5><i class="fas fa-rupee-sign"></i>&nbsp;<?php echo end($gtotal); ?></h5>
+					  </td>
+					  <td>
+					  
+					  </td>
+					  <td></td>
+					  
+					  <td style="padding:30px; text-align:center;">
+					  <a href="order.php?cust_id=<?php echo $cust_id; ?>"><button type="button" style=" color:white; font-weight:bold; text-transform:uppercase;" class="btn btn-warning">Proceed to checkout</button></a>
+					  </td>
+					  <td></td>
+					  </tr>
+						
+						<?php
+	
+	                  }
+					  else
+					  {
+						  
+	
+	                 ?>
+					 <tr><button type="button" class="btn btn-outline-success"><a href="../index.php" style="color:green; text-decoration:none;">No Items In cart Let's Shop Now</a></button></tr>
+					 
+					 <?php
+					  }
+					 ?>
+                 </tbody>
+	      </table>	
+		  
+		  <span style="color:green; text-align:centre;"><?php if(isset($success)) { echo $success; }?></span>
+		       
+			   
+		 
+		
+         </div>	 
+	  
+<!--tab 1 ends-->	   
+			
+			
+			<!--tab 2 starts-->
+            <div class="tab-pane fade" id="manageaccount" role="tabpanel" aria-labelledby="manageaccount-tab">
+			    <form method="post" enctype="multipart/form-data">
+                    <div class="form-group">
+                      <label for="name">Name</label>
+                      <input type="text" id="name" value="<?php if(isset($fld_name)){ echo $fld_name;}?>" class="form-control" name="name" required="required"/>
+                    </div>
+					
+					<div class="form-group">
+                      <label for="email">Email</label>
+                      <input type="email" id="email" name="email" value="<?php if(isset($fld_email)){ echo $fld_email;}?>" class="form-control"  readonly/>
+                    </div>
+					<div class="form-group">
+                      <label for="mobile">Mobile</label>
+                      <input type="tel" id="mobile" class="form-control" name="mobile" pattern="[6-9]{1}[0-9]{2}[0-9]{3}[0-9]{4}" value="<?php if(isset($fld_mobile)){ echo $fld_mobile;}?>" placeholder="" required>
+                    </div>
+					
+                   <div class="form-group">
+                      <label for="pwd">Password:</label>
+                     <input type="password" name="pswd" value="<?php if(isset($fld_password)) { echo $fld_password; }?>"class="form-control"  id="pwd" required/>
+                   </div>
+				   
+				   
+ 
+                  <button type="submit" name="update" style="background:#ED2553; border:1px solid #ED2553;" class="btn btn-primary">Update</button>
+                  <div class="footer" style="color:red;"><?php if(isset($ermsg)) { echo $ermsg; }?><?php if(isset($ermsg2)) { echo $ermsg2; }?></div>
+			 </form>
+			</div>
+			<!--tab 2 ends-->
+			 <!--tab 3 starts-->
+            <div class="tab-pane fade" id="orders" role="tabpanel" aria-labelledby="orders-tab">
+			    <table class="table">
+				<th>Order Number</th>
+				<th>Item Name</th>
+				<th>Price</th>
+				<th>Cancel order</th>
+				    <tbody>
+					<?php
+					$quer_res=mysqli_query($con,"select * from tblorder where fld_email_id='$cust_id' && fldstatus='In Process'");
+					while($roww=mysqli_fetch_array($quer_res))
+					{   
+				         $fid=$roww['fld_food_id'];
+				         $qr=mysqli_query($con,"select * from tbfood where food_id='$fid'");
+						 $qrr=mysqli_fetch_array($qr);
+						
+					  
+					?>
+					   <tr>
+					   <td><?php echo $roww['fld_order_id']; ?></td>
+					   <?php
+					   if(empty($qrr['foodname']))
+					   {
+					   ?>
+					   <td><span style="color:red;">Product Not Available Now</span></td>
+					   <?php
+					   }
+					   else
+					   {
+						   ?>
+						    <td><?php echo $qrr['foodname']; ?></td>
+						   <?php
+					   }
+					   ?>
+					  
+					   <td><?php echo $qrr['cost']; ?></td>
+					   <td><a href="#" onclick="del(<?php echo $roww['fld_order_id'];?>);"><button type="button" class="btn btn-danger">Cancel Order</button></a></td>
+					   </tr>
+					 <?php
+					}
+					 ?>  
+					</tbody>
+				</table>
+			</div>
+			<!--tab 3 ends-->
+	  </div>
+	</div>  
+	  
+<?php
+include("footer.php");
+?>
+
+
+   
+</body>
+</html>
