55a84e328fc03ec843ab9fc842183d1150603e9c
abhisheksingh102 <abhishek.singh_cs17@gla.ac.in>
2020-04-04 22:42:58 +0530

    add function

--- update_food.php
+++ update_food.php
@@ -41,3 +41,14 @@ var h1 = document.getElementsByTagName('h1')[0],
     clear = document.getElementById('clear'),
     seconds = 0, minutes = 0, hours = 0,
     t;
+
+    function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
